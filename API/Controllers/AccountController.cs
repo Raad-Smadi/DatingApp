c7d8af15416a45fc19cf2d09cc36ee0a2286f6af
Raad Smady <3605@rss.jo>
2022-06-13 19:38:26 +0300

    End of section 6

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -26,7 +26,7 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-            if(await UserExists(registerDto.Username)) return BadRequest("Username is take");
+            if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
             using var hmac = new HMACSHA512();
 
             var user = new AppUser
